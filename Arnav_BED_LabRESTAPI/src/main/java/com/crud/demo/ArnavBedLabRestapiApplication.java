654d7c731daa91a74a8bd408b2eafe6ffe6368b1
arnavshil98 <arnv.shil@connaissant.com>
2023-02-05 22:15:16 +0530

    updated project

---
+++ Arnav_BED_LabRESTAPI/src/main/java/com/crud/demo/ArnavBedLabRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.crud.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArnavBedLabRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArnavBedLabRestapiApplication.class, args);
+	}
+
+}
